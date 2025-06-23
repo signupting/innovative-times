90c95d5e6d6052afc24e787a9aeeb1f383623851
signupting <signupting11@gmail.com>
2025-06-24 00:51:05 +0700

    feat: implement preloader for background images and videos

---
+++ js/main.js
@@ -0,0 +1,29 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const backgroundElement = document.querySelector('.relative.overflow-hidden');
+    if (backgroundElement) {
+        const image = backgroundElement.querySelector('img');
+        const video = backgroundElement.querySelector('video');
+
+        if (image) {
+            if (image.complete) {
+                document.body.classList.remove('preloader');
+            } else {
+                image.addEventListener('load', () => {
+                    document.body.classList.remove('preloader');
+                });
+            }
+        } else if (video) {
+            if (video.readyState >= 3) { // HAVE_FUTURE_DATA
+                document.body.classList.remove('preloader');
+            } else {
+                video.addEventListener('canplaythrough', () => {
+                    document.body.classList.remove('preloader');
+                });
+            }
+        } else {
+            document.body.classList.remove('preloader');
+        }
+    } else {
+        document.body.classList.remove('preloader');
+    }
+});
